a21ea9f78901daad5ebbffd82a77e0a5ac675c76
Rodrigues da Silva <rodriguesdasilva@MacBook-Pro-de-Rodrigues.local>
2021-10-22 17:48:53 +0200

    portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,79 @@
-import "./portfolio.scss"
+import "./portfolio.scss";
+import { useEffect, useState } from "react";
+import PortfolioList from "../porfoliolist/PortfolioList";
+
+import {
+  frontendPortfolio,
+  backendPortfolio,
+  otherPortfolio,
+  designPortfolio
+
+} from "../../data";
 
 export default function Portfolio() {
-    return (
-        <div className="portfolio" id="portfolio">
-            
+  const [selected, setSelected] = useState("frontend");
+  const [data, setData] = useState([]);
+
+  const list = [
+    {
+      id: "frontend",
+      title: "Front End Skills",
+    },
+    {
+      id: "backend",
+      title: "Back End Skills",
+    },
+    {
+      id: "ohter",
+      title: "Other Skills",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+   
+  ];
+
+  useEffect(()=>{
+
+    switch(selected){
+      case "frontend":
+        setData(frontendPortfolio);
+        break;
+        case "backend":
+        setData(backendPortfolio);
+        break;
+        case "ohter":
+        setData(otherPortfolio);
+        break;
+        case "web":
+        setData(designPortfolio);
+        break;
+        default:
+          setData(frontendPortfolio);
+    }
+
+  },[selected])
+
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id} />
+        ))}
+      </ul>
+      <div className="container">
+        {data.map((d)=> 
+        <div className="item">
+          <img src={d.img} />
+
+          <h3>{d.title}</h3>
         </div>
-    )
+        )}
+        
+        
+      </div>
+    </div>
+  );
 }
